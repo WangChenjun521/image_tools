bb09fb2938ceeae855f06d95554d3982822a2076
我有点帅哦 <1543995180@qq.com>
2022-08-11 22:47:40 +0800

    update

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from utils import *
 path="image/000000.png"
 
 image=read_image_to_np(path)
-plot_image(image)
+# plot_image(image)
+
+old_path="E:/code/image_tools/image/20220624_131403/depth"
+new_path="E:\code\image_tools/image/20220624_131403_new/depth"
+
+sort_file_and_rename(old_path,new_path)
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ from PIL import Image
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+from shutil import copyfile
 
 def read_image(path):
 	image=Image.open(path)
@@ -19,3 +21,17 @@ def plot_matrix(matrix):
 	fig = plt.figure()
 	sns.heatmap(matrix, annot = True)
 	plt.show()
+def sort_file_and_rename(old_dir,new_dir):
+	# path_romp_pose='D:/论文_元宇宙/code/smpl/data/2022-6-26/test'
+	path_list=os.listdir(old_dir)
+	path_list.sort() #对读取的路径进行排序
+	path_list_new=[]
+	i=0
+	for filename in path_list:
+		# if filename.endswith(".pkl"):
+		# 	path_list_new.append(filename)
+		name,ext = os.path.splitext(filename)
+		copyfile(os.path.join(old_dir,filename), os.path.join(new_dir,"{0:06d}".format(i)+ext))
+		i=i+1
+		# print(os.path.join(path_romp_output,filename))
+	# print(path_list_new)
